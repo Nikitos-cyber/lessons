17bf9f8e21cab8bef508d6e2712c213f8e6dbcda
Nikitos-cyber <golomazdin1212@gmail.com>
2021-05-27 17:36:19 +0300

    First

---
+++ js/script.js
@@ -0,0 +1,3 @@
+  let money, income, addExpenses, deposit, mission, period;
+  alert('Далеко-далеко за словесными горами');
+  console.log('Далеко-далеко за словесными горами, в стране гласных и согласных живут рыбные тексты.');
