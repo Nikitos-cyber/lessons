b43072868305bb623d493a5ac38bad02feaf1589
Nikitos-cyber <golomazdin1212@gmail.com>
2021-05-31 14:52:54 +0300

    homework lesson_3

--- js/script.js
+++ js/script.js
@@ -18,3 +18,50 @@
 
   let budgetDay = 63000 / 30;
   console.log( budgetDay );
+
+  let incom = +prompt('Ваш месячный доход?', '18000'); 
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую ');
+  deposit = confirm('Есть ли у вас депозит в банке?');
+  let expenses1 = prompt('Введите обязательную статью расходов?');
+  let amount1 = +prompt('Во сколько это обойдется?');
+  let expenses2 = prompt('Введите обязательную статью расходов?');
+  let amount2 = +prompt('Во сколько это обойдется?');
+
+  let budgetMonth = incom - (amount1 + amount2);
+  console.log('Бюджет на месяц: ', budgetMonth);
+
+  period = Math.ceil((mission/budgetMonth));
+  let strMonth ='';
+  switch (period){
+    case 1:
+      strMonth = 'а';
+      break;
+
+    case 2:
+      strMonth = 'а';
+      break;
+
+    case 3:
+      strMonth = 'а';
+      break;
+
+    case 4:
+      strMonth = 'а';
+      break;
+    
+    default : strMonth = 'ев';
+  }
+  console.log('Цель будет достигнута за ' + period + ' месяц'+ strMonth);
+
+  budgetDay = Math.floor((budgetMonth/30));
+  console.log('Бюджет на день: ' + budgetDay);
+   
+  if (budgetDay >= 1200){
+    console.log('У вас высокий уровень дохода');
+  }else if ( budgetDay >= 600 && budgetDay < 1200 ){
+    console.log('У вас средний уровень дохода');
+  }else if (budgetDay < 600 && budgetDay >= 0){
+    console.log('К сожалению  у вас низкий уровень дохода');
+  }else {
+    console.log('Что то пошло не так');
+  }
