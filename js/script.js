4d7efc8a68a01ccef901d095a1ed1e70824f6f4b
Nikitos-cyber <golomazdin1212@gmail.com>
2021-06-14 18:32:33 +0300

    homework lesson 15

---
+++ js/script.js
@@ -0,0 +1,24 @@
+  'use strict';
+
+  class First{
+    constructor(){
+
+    }
+
+    hello(){
+      console.log(`Привет я метод родителя!`);
+    }
+  }
+
+
+  class Second extends First {
+    hello(){
+      super.hello();
+
+      console.log(`А я наследуемый метод!`);
+    }
+  }
+
+  const ob1 = new Second();
+  ob1.hello();
+
