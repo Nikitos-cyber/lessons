bd4276f8dda9d43579efa7e0c2fd7fdde808148b
Nikitos-cyber <golomazdin1212@gmail.com>
2021-06-20 22:15:13 +0300

    homework lesson_19 two

--- js/script.js
+++ js/script.js
@@ -1,64 +1,73 @@
+  'use strict';
+
+  let select = document.querySelector('#country');
+  let selectTwo = document.querySelector('#city');
   
-let inputA = document.querySelector('#a');
-let inputB = document.querySelector('#b');
-let sumResult = document.querySelector('#sum');
-let multResult = document.querySelector('#mult');
-let res = document.querySelectorAll('#res');
 
-
- let sum = function (a,b){
-   return a + b;
- };
-
- let mult = function (a,b){
-   return a * b;
- };
-
-let show = function (){
-  sumResult.addEventListener('click',function(){
-    res.forEach((item,i)=>{
-     if(  sum(+inputA.value,+inputB.value) !== item.value){
-      res[0].value = sum(+inputA.value,+inputB.value) ;
+  function enter(){
+    if (select.value === 'rus' ){
+      selectTwo.textContent = '';
+      selectTwo.insertAdjacentHTML('beforeend',`
+      <option value="rus">Москва</option>
+      <option value="uk">Санкт-Петербург</option>
+      <option value="bel">Новосибирск</option>
+      <option value="jap">Екатеренбург</option>
+      <option value="bel">Нижний Новгород</option>
+      <option value="jap">Kазань</option>
+      <option value="bel">Челябинск</option>
       
-     }
-    });
-  });
-
-  multResult.addEventListener('click',function(){
-    res.forEach((item,i)=>{
-     if(  mult(+inputA.value,+inputB.value) !== item.value){
-      res[0].value = mult(+inputA.value,+inputB.value) ;
+      `);
+    
+     
+    }
+     if (select.value === 'uk' ){
+      selectTwo.textContent = '';
+      selectTwo.insertAdjacentHTML('beforeend',`
+      <option value="rus">Киев</option>
+      <option value="uk">Харьков</option>
+      <option value="bel">Одесса</option>
+      <option value="jap">Днепр</option>
+      <option value="bel">Донецк</option>
+      <option value="jap">Запорожец</option>
+      <option value="bel">Львов</option>
       
-     }
-    });
-  });
+      `);
+      
+    }
 
-};
+     if (select.value === 'jap'  ){
+      selectTwo.textContent = '';
+      selectTwo.insertAdjacentHTML('beforeend',`
+      <option value="rus">Токио</option>
+      <option value="uk">Киото</option>
+      <option value="bel">Осака</option>
+      <option value="jap">Икогама</option>
+      
+      
+      `);
+    }
+     if (select.value === 'bel'  ){
+      selectTwo.textContent = '';
+      selectTwo.insertAdjacentHTML('beforeend',`
+      <option value="rus">Минск</option>
+      <option value="uk">Гомель</option>
+     
+      
+      
+      `);
+    }
+  }
 
-show();
+  function check(){
+     let val = document.createElement('p');
+    val.textContent = '';
+    selectTwo.insertAdjacentElement('afterend',val);
+    select.addEventListener('change', enter);
+    selectTwo.addEventListener('change',(event)=>{
+     val.textContent = ` ${select.selectedOptions[0].textContent}, ${selectTwo.selectedOptions[0].textContent}`;
+    })
+  } 
 
 
-let getResult = function(x,y){
-
-  let powNum = Math.pow(x,y);
-  
-
-  let arr = [];
-  
-  do {
-  arr.push(powNum  % 10 );
-  powNum = Math.floor(powNum / 10);
-  
-  }while(powNum > 1);
-  let result = arr.reduce((acum,item)=>{
-    return acum + item;
-  });
-
-  return result;
-
-};
-
-
-console.log( getResult(4,8));
-
+check();
   
