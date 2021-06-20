88ad20c0824d86f4ffb9fcc067bed1437e3800a6
Nikitos-cyber <golomazdin1212@gmail.com>
2021-06-20 19:45:23 +0300

    homework lesson 19 one and two

--- js/script.js
+++ js/script.js
@@ -1,67 +1,64 @@
- 'use strict';
- 
- function timer(){
-
-    let date = new Date().getTime(),
-    date1  =  new Date(),
-    year = date1.getFullYear() + 1,
-    newYear = new Date(` 1 january ${year}`).getTime(),
-    timeRemaining = (newYear - date) / 1000 ,
-    day = Math.ceil(timeRemaining / 60 /60 /24 % 365 ),
-    look  = date1.getHours(),
-    week = ['Воскресенье','Пондельник','Вторник','Среда','Четверг','Пятница','Субота'],
-    hours = date1.getHours();
-    let ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; 
-    
-    let minutes = date1.getMinutes();
-    let seconds = date1.getSeconds();
-    
-    function check (val){
-        let lenght = 0;
-        let num = val ;
-        do{
-            lenght++;
-            num = num / 10;
-
-        }while(num >= 1);
-        if( lenght < 2){
-            val = '0' + val;
-        }
-        return val;
-    }
-    
-    minutes = check(minutes);
-    hours = check(hours);
-     seconds =check(seconds);
-    let nowTime = 'Текущее вермя : ' + hours + ':' + minutes + ':' + seconds + ' ' + ampm;
-   
-   
-
-    if (look < 7 || look > 22){
-        document.write('Доброй ночи');
-    }else if(look >= 7 && look < 11){
-         document.write('Доброе утро');
-    }else if (look >= 11 && look < 17){
-         document.write('Добрый день');
-    }else if(look >= 17 && look <22){
-        document.write('Добрый вечер');
-    }
-
-    function getWeekDay (date){
-        return week[date.getDay()];
-    }
-
-    document.write('<br> Сегодня : ',getWeekDay(date1));
-     document.write('<br>' + nowTime);
-      document.write('<br> До нового года осталось : ' + day + ' дней');
-
-    
-
-    
+  
+let inputA = document.querySelector('#a');
+let inputB = document.querySelector('#b');
+let sumResult = document.querySelector('#sum');
+let multResult = document.querySelector('#mult');
+let res = document.querySelectorAll('#res');
 
 
-    
-}
-timer();
+ let sum = function (a,b){
+   return a + b;
+ };
+
+ let mult = function (a,b){
+   return a * b;
+ };
+
+let show = function (){
+  sumResult.addEventListener('click',function(){
+    res.forEach((item,i)=>{
+     if(  sum(+inputA.value,+inputB.value) !== item.value){
+      res[0].value = sum(+inputA.value,+inputB.value) ;
+      
+     }
+    });
+  });
+
+  multResult.addEventListener('click',function(){
+    res.forEach((item,i)=>{
+     if(  mult(+inputA.value,+inputB.value) !== item.value){
+      res[0].value = mult(+inputA.value,+inputB.value) ;
+      
+     }
+    });
+  });
+
+};
+
+show();
+
+
+let getResult = function(x,y){
+
+  let powNum = Math.pow(x,y);
+  
+
+  let arr = [];
+  
+  do {
+  arr.push(powNum  % 10 );
+  powNum = Math.floor(powNum / 10);
+  
+  }while(powNum > 1);
+  let result = arr.reduce((acum,item)=>{
+    return acum + item;
+  });
+
+  return result;
+
+};
+
+
+console.log( getResult(4,8));
+
+  
